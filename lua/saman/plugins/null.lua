45762950f4bd7f6c726567c673defcd4082e3d0c
Saman <tradeerha@gmail.com>
2023-09-13 17:28:05 +0330

    a

--- lua/saman/plugins/null.lua
+++ lua/saman/plugins/null.lua
@@ -23,7 +23,6 @@ null_ls.setup({
 		formatting.goimports,
 		formatting.golines,
 		formatting.black,
-		diagnostics.mypy,
 		diagnostics.ruff,
 		diagnostics.eslint_d.with({ -- js/ts linter
 			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
