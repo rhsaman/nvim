8301f89abaeabfaa56f362f9ba6ab8416d1065ea
Saman <tradeerha@gmail.com>
2023-10-18 01:49:12 +0330

    enter autocompletion

--- lua/saman/plugins/cmp.lua
+++ lua/saman/plugins/cmp.lua
@@ -31,11 +31,11 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-u>"] = cmp.mapping.scroll_docs(4),
+				["<C-i>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), --enter autocompletion
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
