715935a1374a7ea0b1c3fc369cf2791f49d44355
Saman <tradeerha@gmail.com>
2023-11-04 18:39:48 +0330

    s

--- lua/saman/plugins/colors.lua
+++ lua/saman/plugins/colors.lua
@@ -1,27 +1,26 @@
 return {
-	-- {
-	-- 	"bluz71/vim-nightfly-guicolors",
+	-- 	tokyonight
+	--
+	-- 	"folke/tokyonight.nvim",
 	-- 	priority = 1000, -- make sure to load this before all the other start plugins
 	-- 	config = function()
+	-- 		require("tokyonight").setup({
+	-- 			style = "night",
+	-- 			transparent = true,
+	-- 		})
 	-- 		-- load the colorscheme here
-	-- 		vim.cmd([[colorscheme nightfly]])
+	-- 		vim.cmd([[colorscheme tokyonight]])
 	-- 	end,
-	-- },
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("tokyonight").setup({
-				style = "night",
-				transparent = true,
-			})
-			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
-		end,
-	},
-	-- "morhetz/gruvbox",
-	-- priority = 1000,
-	-- config = function()
-	-- 	vim.cmd.colorscheme("gruvbox")
-	-- end,
+
+	-- 	gruvbox
+	--
+	"morhetz/gruvbox",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("gruvbox")
+
+		-- transparent background
+		vim.cmd("let g:gruvbox_transparent_bg = 1")
+		vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+	end,
 }
