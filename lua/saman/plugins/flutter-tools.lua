ae61f219e3ef50465cb1b8978d2090d308e7d107
Saman <tradeerha@gmail.com>
2023-12-13 17:51:50 +0330

    flutter tools color

--- lua/saman/plugins/flutter-tools.lua
+++ lua/saman/plugins/flutter-tools.lua
@@ -76,9 +76,9 @@ return {
 			lsp = {
 				color = { -- show the derived colours for dart variables
 					enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-					background = true, -- highlight the background
+					background = false, -- highlight the background
 					background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-					foreground = true, -- highlight the foreground
+					foreground = false, -- highlight the foreground
 					virtual_text = true, -- show the highlight using virtual text
 					virtual_text_str = "■", -- the virtual text character to highlight
 				},
