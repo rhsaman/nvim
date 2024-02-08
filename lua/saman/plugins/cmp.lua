4f58d67a50a4cf589654efc9f91804317dcf14a0
Saman <tradeerha@gmail.com>
2024-02-08 15:56:01 +0330

    notification off and cmp tab fix

--- lua/saman/plugins/cmp.lua
+++ lua/saman/plugins/cmp.lua
@@ -20,7 +20,13 @@ return {
 		require("luasnip.loaders.from_vscode").lazy_load()
 
 		local feedkey = function(key, mode)
+			unpack = unpack or table.unpack
 			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+		end
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 		end
 
 		cmp.setup({
@@ -44,15 +50,14 @@ return {
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
-					elseif vim.fn["vsnip#available"](1) == 1 then
-						feedkey("<Plug>(vsnip-expand-or-jump)", "")
+					elseif vim.fn["vsnip#jumpable"](1) == 1 then
+						feedkey("<Plug>(vsnip-jump-next)", "")
 					elseif has_words_before() then
 						cmp.complete()
 					else
 						fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 					end
 				end, { "i", "s" }),
-
 				["<S-Tab>"] = cmp.mapping(function()
 					if cmp.visible() then
 						cmp.select_prev_item()
--- lua/saman/plugins/noice.lua
+++ lua/saman/plugins/noice.lua
@@ -11,7 +11,7 @@ return -- lazy.nvim
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		-- "rcarriga/nvim-notify",
 	},
 
 	config = function()
@@ -32,7 +32,7 @@ return -- lazy.nvim
 			-- you can enable a preset for easier configuration
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
+				command_palette = false, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = true, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
