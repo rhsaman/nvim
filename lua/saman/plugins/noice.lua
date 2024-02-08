43b3eebec88125aa6d31b3598a9d255af11633c6
Saman <tradeerha@gmail.com>
2024-02-08 16:20:38 +0330

    fi

--- lua/saman/plugins/noice.lua
+++ lua/saman/plugins/noice.lua
@@ -20,7 +20,7 @@ return -- lazy.nvim
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = false,
+					["vim.lsp.util.stylize_markdown"] = true,
 					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
 			},
