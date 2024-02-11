389883737a0af8d4689e55b2813c01b6786b73b0
rhsaman <tradeerha@gmail.com>
2024-02-12 02:09:09 +0330

    u

--- lua/saman/plugins/ufo.lua
+++ lua/saman/plugins/ufo.lua
@@ -1,7 +1,7 @@
 return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = "kevinhwang91/promise-async",
-	event = { "BufReadPre", "BufNewFile" },
+	-- event = { "BufReadPre", "BufNewFile" },
 
 	config = function()
 		vim.o.foldcolumn = "0"
