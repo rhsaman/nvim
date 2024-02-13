b659aea11b40a77cfb38fa8035aa81e7dfd83e61
rhsaman <tradeerha@gmail.com>
2024-02-14 01:56:33 +0330

    notify

--- lua/saman/plugins/noice.lua
+++ lua/saman/plugins/noice.lua
@@ -1,46 +1,9 @@
-return -- lazy.nvim
-{
+return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
 	},
-	-- opts = {
-	-- 	cmdline = {
-	-- 		enabled = true, -- enables the Noice cmdline UI
-	-- 		view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-	-- 		opts = {}, -- global options for the cmdline. See section on views
-	-- 		format = {
-	-- 			cmdline = { pattern = "^:", icon = "", lang = "vim" },
-	-- 			search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-	-- 			search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-	-- 			filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-	-- 			lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-	-- 			help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
-	-- 			input = {}, -- Used by input()
-	-- 		},
-	-- 	},
-	-- 	messages = {
-	-- 		enabled = false, -- enables the Noice messages UI
-	-- 	},
-	-- 	popupmenu = {
-	-- 		enabled = false, -- enables the Noice popupmenu UI
-	-- 	},
-	-- 	notify = {
-	-- 		enabled = false,
-	-- 		view = "notify",
-	-- 	},
-	-- 	health = {
-	-- 		checker = false, -- Disable if you don't want health checks to run
-	-- 	},
-	-- 	smart_move = {
-	-- 		enabled = true, -- you can disable this behaviour here
-	-- 		excluded_filetypes = { "cmp_menu", "cmp_docs", "notify" },
-	-- 	},
-	-- },
-	-- add any options here
-
 	config = function()
 		require("noice").setup({
 			lsp = {
@@ -48,7 +11,6 @@ return -- lazy.nvim
 					enabled = false,
 				},
 
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
@@ -58,8 +20,8 @@ return -- lazy.nvim
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
 				command_palette = false, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = true, -- enables an input dialog for inc-rename.nvim
+				long_message_to_split = false, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 		})
---
+++ lua/saman/plugins/notify.lua
@@ -0,0 +1,19 @@
+return {
+	"rcarriga/nvim-notify",
+	config = function()
+		require("notify").setup({
+			top_down = false,
+			stages = "slide",
+			timeout = 1000,
+			render = "wrapped-compact",
+			fps = 30,
+			icons = {
+				DEBUG = "",
+				ERROR = "",
+				INFO = "",
+				TRACE = "✎",
+				WARN = "",
+			},
+		})
+	end,
+}
