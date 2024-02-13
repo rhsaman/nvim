b0eae236268e645194a92764eaeddd28acc0bda8
rhsaman <tradeerha@gmail.com>
2024-02-13 12:47:50 +0330

    flowterm added

---
+++ lua/saman/plugins/flowterm.lua
@@ -0,0 +1,9 @@
+return {
+	"voldikss/vim-floaterm",
+	keys = {
+		{ "<leader>F", ":FloatermNew --height=1.0 --width=1.0 lazygit<cr>", desc = "open flot window" },
+	},
+	-- config=function ()
+	--       vim.g:floaterm_keymap_new = '<Leader>ft'
+	-- end
+}
