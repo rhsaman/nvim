8ec00aa99f84614e60a016a1a9c69c30505d94c1
rhsaman <tradeerha@gmail.com>
2024-02-20 21:50:47 +0330

    indent color

--- lua/saman/plugins/blankindent.lua
+++ lua/saman/plugins/blankindent.lua
@@ -3,9 +3,18 @@ return {
 	event = "VeryLazy",
 	main = "ibl",
 	opts = {},
+
 	config = function()
+		local highlight = { "gray" }
+		local hooks = require("ibl.hooks")
+
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "gray", { fg = "#313030" })
+		end)
+
 		require("ibl").setup({
 			scope = { enabled = false },
+			indent = { highlight = highlight },
 		})
 	end,
 }
