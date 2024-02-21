ce0387114acdb4b04e42a28853f2f613d4c195e4
rhsaman <tradeerha@gmail.com>
2024-02-21 23:53:18 +0330

    neotree

--- lua/saman/plugins/neotree.lua
+++ lua/saman/plugins/neotree.lua
@@ -85,7 +85,7 @@ return {
 				-- If you don't want to use these columns, you can set `enabled = false` for each of them individually
 				file_size = {
 					enabled = true,
-					required_width = 64, -- min width of window required to show this column
+					required_width = 110, -- min width of window required to show this column
 				},
 				type = {
 					enabled = true,
@@ -93,7 +93,7 @@ return {
 				},
 				last_modified = {
 					enabled = true,
-					required_width = 88, -- min width of window required to show this column
+					required_width = 64, -- min width of window required to show this column
 				},
 				created = {
 					enabled = true,
