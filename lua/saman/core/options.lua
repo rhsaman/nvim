ec2b0cf60933ec686db1bfe17428f850ce35a850
rhsaman <tradeerha@gmail.com>
2024-03-13 14:27:10 +0330

    fold color and ufo deleted

--- lua/saman/core/options.lua
+++ lua/saman/core/options.lua
@@ -40,7 +40,7 @@ vim.g.vim_markdown_frontmatter = 0
 -- vim.g.python3_host_prog = vim.fn.expand("~/Documents/code/kittron/back/py/venv/bin/python3")
 
 -- vim-rooter
-vim.cmd("let g:rooter_patterns = ['.git', 'Makefile', '*.sln', 'build/env.sh','main.go']")
+vim.cmd("let g:rooter_patterns = ['.git', 'Makefile','main.go','pubspec.yaml','README.md']")
 
 -- fold
 vim.cmd([[
--- lua/saman/plugins/gruvbox.lua
+++ lua/saman/plugins/gruvbox.lua
@@ -3,11 +3,15 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme("gruvbox-material")
+
 		-- markdown ui
 		vim.cmd("hi @markup.raw.delimiter guifg=gray")
 		vim.cmd("hi @conceal guifg=gray")
 		vim.cmd("hi @h1 guifg=#B4D797")
 		vim.cmd("hi @head guifg=white")
 		vim.cmd("hi @title guifg=white")
+
+		-- fold color
+		vim.cmd("hi Folded guibg=#9F834D guifg=black")
 	end,
 }
