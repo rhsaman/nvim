b570b04960520efe6f3940f3c0fc4662c239b8c3
rhsaman <tradeerha@gmail.com>
2024-04-15 16:11:48 +0330

    vim root added

--- lua/saman/plugins/telescope.lua
+++ lua/saman/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
-	event = { "BufReadPre", "BufNewFile" },
+	-- event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{
