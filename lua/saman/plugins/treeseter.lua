0e9f948799c19d8568b63746bdc3ad036aa99389
rhsaman <tradeerha@gmail.com>
2024-05-24 01:04:39 +0330

    up

--- lua/saman/plugins/treeseter.lua
+++ lua/saman/plugins/treeseter.lua
@@ -7,13 +7,14 @@ return {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 			"windwp/nvim-ts-autotag",
 			"HiPhish/nvim-ts-rainbow2",
-			"nvim-treesitter/playground",
 		},
 
 		config = function()
 			local treesitter = require("nvim-treesitter.configs")
+			-- local rainbow = require("ts-rainbow")
 
-			treesitter.setup({
+			-- configure treesitter
+			treesitter.setup({ -- enable syntax highlighting
 				rainbow = {
 					enable = true,
 					query = {
@@ -23,83 +24,15 @@ return {
 					},
 					-- strategy = rainbow.strategy.global,
 				},
-
-				highlight = { enable = true },
-
-				auto_install = true,
-
-				indent = { enable = true },
-
-				incremental_selection = {
+				highlight = {
 					enable = true,
+					additional_vim_regex_highlighting = false,
 				},
-
-				-- autotag
-				require("nvim-treesitter.configs").setup({
-					autotag = {
-						enable = true,
-						enable_rename = true,
-						enable_close = true,
-						enable_close_on_slash = true,
-						filetypes = {
-							"html",
-							"javascript",
-							"typescript",
-							"javascriptreact",
-							"typescriptreact",
-							"svelte",
-							"vue",
-							"tsx",
-							"jsx",
-							"rescript",
-							"xml",
-							"php",
-							"markdown",
-							"astro",
-							"glimmer",
-							"handlebars",
-							"hbs",
-						},
-						skip_tags = {
-							"area",
-							"base",
-							"br",
-							"col",
-							"command",
-							"embed",
-							"hr",
-							"img",
-							"slot",
-							"input",
-							"keygen",
-							"link",
-							"meta",
-							"param",
-							"source",
-							"track",
-							"wbr",
-							"menuitem",
-						},
-					},
-				}),
-
-				playground = {
+				-- enable indentation
+				indent = { enable = true },
+				-- enable autotagging (w/ nvim-ts-autotag plugin)
+				autotag = {
 					enable = true,
-					disable = {},
-					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = "o",
-						toggle_hl_groups = "<leader>ti",
-						toggle_injected_languages = "t",
-						toggle_anonymous_nodes = "a",
-						toggle_language_display = "I",
-						focus_language = "f",
-						unfocus_language = "F",
-						update = "R",
-						goto_node = "<cr>",
-						show_help = "?",
-					},
 				},
 				-- ensure these language parsers are installed
 				ensure_installed = {
@@ -125,7 +58,24 @@ return {
 					"go",
 					"dart",
 				},
+
+				incremental_selection = {
+					enable = true,
+				},
 				-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+				ts_context_commentstring = {
+					enable = true,
+					enable_autocmd = false,
+					config = {
+						javascript = {
+							__default = "// %s",
+							jsx_element = "{/* %s */}",
+							jsx_fragment = "{/* %s */}",
+							jsx_attribute = "// %s",
+							comment = "// %s",
+						},
+					},
+				},
 			})
 		end,
 	},
