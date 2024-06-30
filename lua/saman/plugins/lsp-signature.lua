183b786a68c19c1e079bdfcb3a99b57dc9ac1656
rhsaman <tradeerha@gmail.com>
2024-06-30 10:52:37 +0330

    update

--- lua/saman/plugins/lsp-signature.lua
+++ lua/saman/plugins/lsp-signature.lua
@@ -7,11 +7,11 @@ return {
 			shadow_blend = 36, -- if you using shadow as border use this set the opacity
 			shadow_guibg = "Green", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
 			timer_interval = 201, -- default timer check interval set to lower value if you want to reduce latency
-			toggle_key = "<c-o>", -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+			toggle_key = "<c-k>", -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 			toggle_key_flip_floatwin_setting = true, -- true: toggle float setting after toggle key pressed
 			select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
 			move_cursor_key = "<c-e>", -- imap, use nvim_set_current_win to move cursor between current win and floating
-			auto_close_after = 10000,
+			auto_close_after = 20000,
 
 			floating_window_off_x = 5, -- adjust float windows x position.
 			floating_window_off_y = function() -- adjust float windows y position. e.g. set to -2 can make floating window move up 2 lines
@@ -34,8 +34,8 @@ return {
 		}
 		require("lsp_signature").setup(cfg) -- no need to specify bufnr if you don't use toggle_key
 
-		vim.keymap.set({ "n" }, "<Leader>o", function()
-			vim.lsp.buf.signature_help()
+		vim.keymap.set({ "n" }, "<Leader>k", function()
+			require("lsp_signature").toggle_float_win()
 		end, { silent = true, noremap = true, desc = "toggle signature" })
 	end,
 }
