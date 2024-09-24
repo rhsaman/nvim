0f00dc25a48e2e6d71e03407f617066f2d6c50ec
rhsaman <tradeerha@gmail.com>
2024-09-24 13:27:05 +0330

    up

--- lua/saman/plugins/auto-session.lua
+++ lua/saman/plugins/auto-session.lua
@@ -1,25 +1,25 @@
 return {
 	"rmagatti/auto-session",
-	-- event = { "BufReadPre", "BufNewFile" },
+	event = "VimEnter", -- change event to "VimEnter" for auto-session
 	config = function()
 		local auto_session = require("auto-session")
 
 		auto_session.setup({
-			silent_restore = true,
+			-- silent_restore = true,
 			auto_session_enable_last_session = true,
 			auto_session_enabled = true,
 			auto_save_enabled = true,
-			auto_restore_enabled = true,
+			-- auto_restore_enabled = true,
 			log_level = "error",
 			auto_session_suppress_dirs = {
 				"~/",
-				"/",
 				"~/Dev/",
 				"~/Documents",
-				"~/code",
 				"~/.config",
 			},
 		})
-		-- vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+		-- If you intend to apply session options:
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 	end,
 }
