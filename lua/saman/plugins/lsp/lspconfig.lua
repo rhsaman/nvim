026d4aa97ae500cd947a707f95ddc3cecb30d9ec
rhsaman <tradeerha@gmail.com>
2024-10-18 13:16:28 +0330

    up

--- lua/saman/plugins/lsp/lspconfig.lua
+++ lua/saman/plugins/lsp/lspconfig.lua
@@ -166,12 +166,12 @@ return {
 			on_attach = on_attach,
 			settings = {
 				dart = {
-					dartExcludedFolders = {
-						vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
-						vim.fn.expand("$HOME/.pub-cache"),
-						vim.fn.expand("/opt/homebrew/"),
-						vim.fn.expand("$HOME/tools/flutter/"),
-					},
+					-- dartExcludedFolders = {
+					-- 	vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
+					-- 	vim.fn.expand("$HOME/.pub-cache"),
+					-- 	vim.fn.expand("/opt/homebrew/"),
+					-- 	vim.fn.expand("$HOME/tools/flutter/"),
+					-- },
 				},
 			},
 		})
