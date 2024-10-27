881ad9c268aee474b847168b22901fd8650b1c7b
rhsaman <tradeerha@gmail.com>
2024-10-27 14:17:07 +0330

    up

--- lua/saman/plugins/codum.lua
+++ lua/saman/plugins/codum.lua
@@ -1,23 +1,23 @@
 return {
-	"Exafunction/codeium.vim",
-	config = function()
-		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set("i", "<c-CR>", function()
-			return vim.fn["codeium#Accept"]()
-		end, { expr = true, silent = true })
+  "Exafunction/codeium.vim",
+  config = function()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set("i", "<c-a>", function()
+      return vim.fn["codeium#Accept"]()
+    end, { expr = true, silent = true })
 
-		vim.keymap.set("i", "<c-j>", function()
-			return vim.fn["codeium#CycleCompletions"](1)
-		end, { expr = true, silent = true })
+    vim.keymap.set("i", "<c-n>", function()
+      return vim.fn["codeium#CycleCompletions"](1)
+    end, { expr = true, silent = true })
 
-		vim.keymap.set("i", "<c-k>", function()
-			return vim.fn["codeium#CycleCompletions"](-1)
-		end, { expr = true, silent = true })
+    vim.keymap.set("i", "<c-p>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true, silent = true })
 
-		vim.keymap.set("i", "<c-x>", function()
-			return vim.fn["codeium#Clear"]()
-		end, { expr = true, silent = true })
+    vim.keymap.set("i", "<c-x>", function()
+      return vim.fn["codeium#Clear"]()
+    end, { expr = true, silent = true })
 
-		vim.keymap.set("n", "<leader>cc", ":CodeiumChat<CR>", { desc = "ai chat" })
-	end,
+    vim.keymap.set("n", "<leader>cc", ":CodeiumChat<CR>", { desc = "ai chat" })
+  end,
 }
