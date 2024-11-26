72c72c5883313a81ba3f30599a7c9ba0ef4eed69
rhsaman <tradeerha@gmail.com>
2024-11-26 15:08:14 +0330

    up

--- lua/saman/plugins/cmp.lua
+++ lua/saman/plugins/cmp.lua
@@ -17,47 +17,6 @@ return {
 		-- Load snippets
 		require("luasnip.loaders.from_vscode").lazy_load()
 
-		-- Global Snippets
-		-- luasnip.add_snippets("all", {
-		-- 	luasnip.s(
-		-- 		"func",
-		-- 		luasnip.fmt("function {}({})\n  {}\nend", {
-		-- 			luasnip.i(1, "function_name"), -- Placeholder for function name
-		-- 			luasnip.i(2), -- Placeholder for parameters
-		-- 			luasnip.i(3), -- Placeholder for body
-		-- 		})
-		-- 	),
-		-- 	luasnip.s(
-		-- 		"class",
-		-- 		luasnip.fmt("class {}({})\n  {}\nend", {
-		-- 			luasnip.i(1, "ClassName"), -- Placeholder for class name
-		-- 			luasnip.i(2), -- Placeholder for parent class
-		-- 			luasnip.i(3), -- Placeholder for class body
-		-- 		})
-		-- 	),
-		-- 	luasnip.s(
-		-- 		"forloop",
-		-- 		luasnip.fmt("for {} = {}, {} do\n  {}\nend", {
-		-- 			luasnip.i(1, "i"), -- Iterator
-		-- 			luasnip.i(2, "1"), -- Start value
-		-- 			luasnip.i(3, "10"), -- End value
-		-- 			luasnip.i(4), -- Loop body
-		-- 		})
-		-- 	),
-		-- 	luasnip.s(
-		-- 		"ifelse",
-		-- 		luasnip.fmt("if {} then\n  {}\nelse\n  {}\nend", {
-		-- 			luasnip.i(1, "condition"), -- Placeholder for condition
-		-- 			luasnip.i(2), -- Placeholder for "if" block
-		-- 			luasnip.i(3), -- Placeholder for "else" block
-		-- 		})
-		-- 	),
-		-- 	luasnip.s(
-		-- 		"print",
-		-- 		luasnip.fmt('print("{}")', { luasnip.i(1, "message") }) -- Placeholder for print message
-		-- 	),
-		-- })
-
 		cmp.setup({
 			completion = {
 				completeopt = "menu,menuone,noselect",
@@ -68,6 +27,13 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- Previous suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- Next suggestion
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-u>"] = cmp.mapping.scroll_docs(4),
+				["<C-o>"] = cmp.mapping.complete(), -- Show suggestions
+				["<C-e>"] = cmp.mapping.abort(), -- Close suggestions
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection
 				-- Keep <Tab> as a fallback for default behavior
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if luasnip.expand_or_jumpable() then
@@ -83,13 +49,6 @@ return {
 						fallback() -- Perform normal shift-tabbing
 					end
 				end, { "i", "s" }),
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- Previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- Next suggestion
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-u>"] = cmp.mapping.scroll_docs(4),
-				["<C-i>"] = cmp.mapping.complete(), -- Show suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- Close suggestions
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection
 			}), -- Sources for autocompletion
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
